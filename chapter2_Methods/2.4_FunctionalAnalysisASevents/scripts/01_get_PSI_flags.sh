ea3ce7f22c3c9b7cce2d56db2bb4ece2a4d05305
larisa-msoto <larisa.moralessoto@mail.mcgill.ca>
2024-02-12 16:04:22 -0500

    scripts section 2.4

---
+++ chapter2_Methods/2.4_FunctionalAnalysisASevents/scripts/01_get_PSI_flags.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/bash
+#SBATCH --job-name="flag"
+#SBATCH --err=logs/flag.%j.err
+#SBATCH --output=outs/flag.%j.out
+#SBATCH --account=rrg-hsn
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=30gb
+#SBATCH --time=01:00:00
+#SBATCH --array=0-139
+
+# Load modules
+
+module load StdEnv/2020 r/4.1.2
+
+# Define variables
+
+CTS_DIR=~/lmprojects/hgthesis_lms/chapter2_Methods/2.1_TRexMethod/tcga/output
+OUT_DIR=../output/psi_flags
+
+params=()
+i=0
+while read p
+do
+  params[i]="$p"
+  i=$((i+1))      
+done < flag_params.tsv
+
+cancer=$(echo ${params[${SLURM_ARRAY_TASK_ID}]} | cut -d "-" -f1 )
+event=$(echo ${params[${SLURM_ARRAY_TASK_ID}]} | cut -d "-" -f2 )
+
+mkdir -p ${OUT_DIR}
+
+echo "Input settings:"
+echo "- Cancer = $cancer"
+echo "- Events = $event"
+
+Rscript get_psi_flags.r ${cancer} ${event} ${CTS_DIR} ${OUT_DIR}
